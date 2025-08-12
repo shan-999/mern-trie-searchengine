21d96eb84a40c2f66d1e5fac81669715619c1add
Muhammed Shan <shanushan73011@gmail.com>
2025-08-13 01:08:12 +0530

    change frondent url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstence = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL_LOCAL,
+    baseURL:import.meta.env.VITE_API_BASE_URL,
     headers:{
         "Content-Type": "application/json",
     }
