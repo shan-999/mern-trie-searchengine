813dcb82f93492894bc6b5a7b03227c3decf7556
Muhammed Shan <shanushan73011@gmail.com>
2025-08-14 21:50:40 +0530

    change the url local to remote

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_API_BASE_URL_LOCAL)
+console.log(import.meta.env.VITE_API_BASE_URL)
 const axiosInstence = axios.create({
     baseURL:import.meta.env.VITE_API_BASE_URL,
     headers:{
